675c7d4ff42a3335aeb04ef48e5809a3e1c918c4
OscarLight <oscarli2716@gmail.com>
2017-11-04 03:10:37 -0400

    commit

---
+++ src-cuda/shallow2d.cu
@@ -0,0 +1,93 @@
+#include <string.h>
+#include <math.h>
+
+#ifndef RESTRICT
+#define restrict __restrict__
+#endif /* RESTRICT */
+//ldoc on
+/**
+ * ## Implementation
+ *
+ * The actually work of computing the fluxes and speeds is done
+ * by local (`static`) helper functions that take as arguments
+ * pointers to all the individual fields.  This is helpful to the
+ * compilers, since by specifying the `restrict` keyword, we are
+ * promising that we will not access the field data through the
+ * wrong pointer.  This lets the compiler do a better job with
+ * vectorization.
+ */
+
+__constant__ static const float g = 9.8;
+
+
+// total number of cells (ncells) = nx_all * ny_all
+__device__ static 
+void shallow2dv_flux(float* restrict fh,
+                     float* restrict fhu,
+                     float* restrict fhv,
+                     float* restrict gh,
+                     float* restrict ghu,
+                     float* restrict ghv,
+                     const float* restrict h,
+                     const float* restrict hu,
+                     const float* restrict hv,
+                     float g,
+                     int ncell)
+{
+    cudaMemcpy(fh, hu, ncell * sizeof(float), cudaMemcpyDeviceToDevice);
+    cudaMemcpy(gh, hv, ncell * sizeof(float), cudaMemcpyDeviceToDevice);
+    const unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    const unsigned int idy = (blockIdx.y * blockDim.y) + threadIdx.y;
+    // linearize to 1D
+    const unsigned int tid = ((gridDim.x * blockDim.x) * idy) + idx;
+    
+    float hi = h[tid], hui = hu[tid], hvi = hv[tid];
+    float inv_h = 1/hi;
+    fhu[tid] = hui*hui*inv_h + (0.5f*g)*hi*hi;
+    fhv[tid] = hui*hvi*inv_h;
+    ghu[tid] = hui*hvi*inv_h;
+    ghv[tid] = hvi*hvi*inv_h + (0.5f*g)*hi*hi;
+}
+
+
+__device__ static
+void shallow2dv_speed(float* restrict cxy,
+                      const float* restrict h,
+                      const float* restrict hu,
+                      const float* restrict hv,
+                      float g,
+                      int ncell)
+{
+    float cx = cxy[0];
+    float cy = cxy[1];
+    const unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    const unsigned int idy = (blockIdx.y * blockDim.y) + threadIdx.y;
+    // linearize to 1D
+    const unsigned int tid = ((gridDim.x * blockDim.x) * idy) + idx;
+    float hi = h[tid];
+    float inv_hi = 1.0f/h[tid];
+    float root_gh = sqrtf(g * hi);
+    float cxi = fabsf(hu[tid] * inv_hi) + root_gh;
+    float cyi = fabsf(hv[tid] * inv_hi) + root_gh;
+    if (cx < cxi) cx = cxi;
+    if (cy < cyi) cy = cyi;
+    cxy[0] = cx;
+    cxy[1] = cy;
+}
+
+__device__
+void shallow2d_flux(float* FU, float* GU, const float* U,
+                    int nx, int ny, int field_stride)
+{
+    shallow2dv_flux<<<nx, ny>>>(FU, FU+field_stride, FU+2*field_stride,
+                    GU, GU+field_stride, GU+2*field_stride,
+                    U,  U +field_stride, U +2*field_stride,
+                    g, nx * ny);
+}
+
+__device__
+void shallow2d_speed(float* cxy, const float* U,
+                     int nx, int ny, int field_stride)
+{
+    shallow2dv_speed<<<nx, ny>>>(cxy, U, U+field_stride, U+2*field_stride, g, nx * ny);
+}
---
+++ src-cuda/shallow2d_test.cu
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "shallow2d.cuh"
+#include "shallow2d.h"
+
+typedef void (*flux_t)(float* FU, float* GU, const float* U,
+                       int nx, int ny, int field_stride);
+typedef void (*speed_t)(float* cxy, const float* U,
+                        int nx, int ny, int field_stride);
+
+__global__ 
+void testShallow2d_by_reference(float* cxy, 
+				float* FU, float* GU, const float* U,
+                int nx, int ny, int field_stride){
+	shallow2d_flux_cu(FU, GU, U, nx, ny, field_stride);
+	shallow2d_speed_cu(cxy, U, nx, ny, field_stride);
+}
+
+void testShallow2d_baseline(float* cxy, 
+				float* FU, float* GU, const float* U,
+                int nx, int ny, int field_stride){
+	shallow2d_flux(FU, GU, U, nx, ny, field_stride);
+	shallow2d_speed(cxy, U, nx, ny, field_stride);
+}
+
+int main(int argc, char** argv){
+	const int nx = 1, ny = 2;
+	const int ncell = nx * ny;
+	count int field_stride = nx * ny;
+	float cxy[2] = {1.0, 2.0};
+	int i;
+	for (i = 0; i < ncell * 3; i++) {
+    	FU[i] = rand();
+    	GU[i] = rand();
+    	U[i] = rand();
+	}
+
+	// Execute baseline code
+	testShallow2d_baseline(cxy, FU, GU, U, nx, ny, field_stride);
+}
