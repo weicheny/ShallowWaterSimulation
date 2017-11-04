409f588a93161ac77c39282752cc85738161168a
OscarLight <oscarli2716@gmail.com>
2017-11-04 14:06:24 -0400

    commit

--- src-cuda/shallow2d_test.cu
+++ src-cuda/shallow2d_test.cu
@@ -34,6 +34,7 @@ void print_array(float* array, int len) {
 
 int main(int argc, char** argv){
 	cudaEvent_t start,stop;
+	float ms;
 	const int nx = 1, ny = 2;
 	const int ncell = nx * ny;
 	const int field_stride = nx * ny;
